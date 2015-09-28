8fc777e0b6e0aae17ceaf0062a7c3540b5d14966
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-09-28 14:25:57 +0200

    Updated bootstrap-ansible.sh
    Clone recipes-rss-ansible from github to control node before executing

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -7,7 +7,7 @@ echo Installing ansible ...
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get -y install ansible
+sudo apt-get -y install ansible git
 
 # Deploy vagrant insecure key
 echo Copying ssh key ...
@@ -15,7 +15,11 @@ cp /vagrant/vagrant_insecure_key /home/vagrant/.ssh/id_rsa
 chmod 600 /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
 
+# Clone recipes-rss-ansible
+cd /tmp
+git clone https://github.com/hora-prediction/recipes-rss-ansible.git
+
 # Execute ansible
 echo Executing ansible ...
-cd /vagrant/rssrecipes-ansible
-ansible-playbook -i ../vagrant_ansible_inventory run.yml
+cd /tmp/recipes-rss-ansible
+ansible-playbook -i /vagrant/vagrant_ansible_inventory run.yml
